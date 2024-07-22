f626d5ce11441cfb6fbae6d0cc4ae724bf555ece
Sandeep Lodhi <90265623+Sandeep-Lodhi@users.noreply.github.com>
2024-07-23 01:41:54 +0530

    Update supabase.js

--- 17-the-wild-oasis/src/services/supabase.js
+++ 17-the-wild-oasis/src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-export const supabaseUrl = "https://dclaevazetcjjkrzczpc.supabase.co";
+export const supabaseUrl = "https://mnjgqslwueevjatrvuid.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRjbGFldmF6ZXRjamprcnpjenBjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMyOTIzNDQsImV4cCI6MTk5ODg2ODM0NH0.LGg0M-taoHgKtxCzr9owrb09epnPaO_Yfz6xVE54sIY";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uamdxc2x3dWVldmphdHJ2dWlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE2Njk4NTYsImV4cCI6MjAzNzI0NTg1Nn0.6ZwVKczQtr5aDgypfyA1i0uTNkFmcXO2EAr8Dwr4Uk0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
